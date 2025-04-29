915703aaaf66c0739550526de9f6748adbfa2428
Tong Li <litongjava001@gmail.com>
2025-04-29 12:43:26 -1000

    update FishableI_a

--- src/main/java/FishableI_a.java
+++ src/main/java/FishableI_a.java
@@ -1,41 +1,41 @@
 /**
-* Abstract parent class for Fishable I_a (fish) species.
-* Subclass of I_a
-* @author Lisa Miller
-* @since 4/23/22
-*/
+ * Abstract parent class for Fishable I_a (fish) species. Subclass of I_a
+ * 
+ * @author Lisa Miller
+ * @since 4/23/22
+ */
 public abstract class FishableI_a extends I_a implements Fishable {
 
-   /**
-   * Constructor for FishableI_a. Just passes parameters through.
-   *@param name This fish type's Hawaiian name.
-   *@param englishName The fish type's English name.
-   *@param scientificName The fish species's scientific name.
-   *@param maxLength The maximum length this name applies.
-   *@param minLength The minimum length this name applies.
-   *@param length This fish's size.
-   *@param weight This fish's weight.
-   *@param diet The diet preference of this fish type.
-   *@param bodyColor The body color of this fish.
-   *@param finColor The fin color of this fish.
-   *@param sex The sex of this fish.
-   *@throws FishSizeException if length exceeds maxLength or is less than minLength
-   */
+	/**
+	 * Constructor for FishableI_a.
+	 * 
+	 * @param name           Hawaiian name.
+	 * @param englishName    English name.
+	 * @param scientificName scientific name.
+	 * @param maxLength      maximum length.
+	 * @param minLength      minimum length.
+	 * @param length         fish's size.
+	 * @param weight         fish's weight.
+	 * @param diet           diet.
+	 * @param bodyColor      body color.
+	 * @param finColor       fin color.
+	 * @param sex            sex.
+	 * @throws FishSizeException FishSizeException.
+	 */
 
-   public  FishableI_a(String name, String englishName, String scientificName,
-         double maxLength, double minLength, double length, double weight, String[] diet, 
-         String bodyColor, String finColor, String sex) {
-         
-         super(name, englishName, scientificName, maxLength, minLength, length, weight,
-               diet, bodyColor, finColor, sex);
-         
-   }
+	public FishableI_a(String name, String englishName, String scientificName, double maxLength,
+			double minLength, double length, double weight, String[] diet, String bodyColor,
+			String finColor, String sex) {
 
+		super(name, englishName, scientificName, maxLength, minLength, length, weight, diet,
+				bodyColor, finColor, sex);
 
-   /** returns new FishableI_a of next level
-   * When a fish reaches maxSize, it should transform to next
-   * level Hawaiian type.
-   * @return a new FishableI_a of the next level type, should replace caller
-   */
-   protected abstract FishableI_a levelUp();
+	}
+
+	/**
+	 * returns new FishableI_a of next level When a fish reaches maxSize.
+	 * 
+	 * @return a new FishableI_a of the next level type, should replace caller
+	 */
+	protected abstract FishableI_a levelUp();
 }
