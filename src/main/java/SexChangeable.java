8510ea5849dc381e514f2b3c29efdf62ba06d16f
Tong Li <litongjava001@gmail.com>
2025-04-29 13:16:55 -1000

    update SexChangeable

--- src/main/java/SexChangeable.java
+++ src/main/java/SexChangeable.java
@@ -1,26 +1,24 @@
 /**
-* interface for I_a that change sex.
-* @author Lisa Miller
-* @since 2/11/22
-*/
+ * interface for I_a that change sex.
+ * 
+ * @author Lisa Miller
+ * @since 2/11/22
+ */
 public interface SexChangeable {
 
-    /* ---- constants ------ */
-   /** List of possible sex changing type. */
-   String[] REPRODUCTIVE_MODES = {"protogynous", "protoandrous", "serial"};
+	/* ---- constants ------ */
+	/** List of possible sex changing type. */
+	String[] REPRODUCTIVE_MODES = { "protogynous", "protoandrous", "serial" };
 
+	/**
+	 * changes this fish's sex different species have different possible changes if
+	 * fish doesn't change sex this method should throw UnsupportedOptionException.
+	 */
+	void changeSex();
 
-   /** 
-   * changes this fish's sex
-   * different species have different possible changes
-   * if fish doesn't change sex this method should throw UnsupportedOptionException
-   */
-   public abstract void changeSex();
-   
-
-   
-   /**
-   * returns the reproductive mode of this sex changing fish
-   */
-   public abstract String getReproductiveMode();
+	/**
+	 * returns the reproductive mode of this sex changing fish.
+	 * @return string.
+	 */
+   String getReproductiveMode();
 }
