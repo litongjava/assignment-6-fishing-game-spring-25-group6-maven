099dcd31fe6ecae9f63f476c0c771ba520af8204
Tong Li <litongjava001@gmail.com>
2025-04-29 12:46:20 -1000

    update FishSexException

--- src/main/java/FishSexException.java
+++ src/main/java/FishSexException.java
@@ -1,15 +1,17 @@
 /**
-* Exception for I_a class sex errors.
-* @author Lisa Miller
-* @since 2/2/24
-*/
+ * Exception for I_a class sex errors.
+ * 
+ * @author Lisa Miller
+ * @since 2/2/24
+ */
 public class FishSexException extends RuntimeException {
-  
-  /**
-  * Constructor. 
-  * @param newMessage The error message to be sent.
-  */
-   public FishSexException(String newMessage) {
-      super(newMessage);
-   }
+
+	/**
+	 * Constructor.
+	 * 
+	 * @param newMessage The error message to be sent.
+	 */
+	public FishSexException(String newMessage) {
+		super(newMessage);
+	}
 }
