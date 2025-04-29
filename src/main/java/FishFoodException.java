d1cad1b58dd89864d87ddd5bbc2d89207e9d81a9
Tong Li <litongjava001@gmail.com>
2025-04-29 12:43:53 -1000

    update FishFoodException

--- src/main/java/FishFoodException.java
+++ src/main/java/FishFoodException.java
@@ -1,15 +1,17 @@
 /**
-* Exception for I_a class food errors.
-* @author Lisa Miller
-* @since 2/11/11
-*/
+ * Exception for I_a class food errors.
+ * 
+ * @author Lisa Miller
+ * @since 2/11/11
+ */
 public class FishFoodException extends RuntimeException {
-  
-  /**
-  * Constructor. 
-  * @param newMessage The error message to be sent.
-  */
-   public FishFoodException(String newMessage) {
-      super(newMessage);
-   }
+
+	/**
+	 * Constructor.
+	 * 
+	 * @param newMessage The error message to be sent.
+	 */
+	public FishFoodException(String newMessage) {
+		super(newMessage);
+	}
 }
