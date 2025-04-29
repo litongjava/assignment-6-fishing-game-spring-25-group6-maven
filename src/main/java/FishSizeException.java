9d6920baeefae8c46cfc1e47e2461c2e931346d3
Tong Li <litongjava001@gmail.com>
2025-04-29 12:46:36 -1000

    update FishSizeException

--- src/main/java/FishSizeException.java
+++ src/main/java/FishSizeException.java
@@ -1,15 +1,17 @@
 /**
-* Exception for I_a class length errors.
-* @author Lisa Miller
-* @since 2/11/11
-*/
+ * Exception for I_a class length errors.
+ * 
+ * @author Lisa Miller
+ * @since 2/11/11
+ */
 public class FishSizeException extends RuntimeException {
-  
-  /**
-  * Constructor. 
-  * @param newMessage The error message to be sent.
-  */
-   public FishSizeException(String newMessage) {
-      super(newMessage);
-   }
+
+	/**
+	 * Constructor.
+	 * 
+	 * @param newMessage The error message to be sent.
+	 */
+	public FishSizeException(String newMessage) {
+		super(newMessage);
+	}
 }
