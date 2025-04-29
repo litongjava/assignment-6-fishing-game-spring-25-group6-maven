96e2ef4bec1a84b47825c29ad40c656052aab276
Tong Li <litongjava001@gmail.com>
2025-04-29 12:35:29 -1000

    update ColorChangeable

--- src/main/java/ColorChangeable.java
+++ src/main/java/ColorChangeable.java
@@ -1,12 +1,12 @@
 /**
-* interface for I_a that change color.
-* @author Lisa Miller
-* @since 2/11/22
-*/
+ * interface for I_a that change color.
+ * 
+ * @author Lisa Miller
+ * @since 2/11/22
+ */
 public interface ColorChangeable {
-
-   /**
-   * Some fish can dynamically change colors, this method should do that.
-   */
-   public abstract void setColor(String bodyColor, String finColor);
+	/**
+	 * Some fish can dynamically change colors, this method should do that.
+	 */
+	public abstract void setColor(String bodyColor, String finColor);
 }
