0f427a313011837f5d1afc62c2630426e22495b7
Tong Li <litongjava001@gmail.com>
2025-05-06 08:52:28 -1000

    update RuleViewer

--- src/main/java/ReelItIn.java
+++ src/main/java/ReelItIn.java
@@ -28,7 +28,7 @@ public class ReelItIn {
 	 * MONTH_NAMES.
 	 */
 	public static final String[] MONTH_NAMES = { "January", "February", "March", "April", "May",
-			"June", "July", "August", "September", "October", "November", "December" };
+		"June", "July", "August", "September", "October", "November", "December" };
 
 	/**
 	 * main method.
